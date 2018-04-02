37f466b29b79c2e58cd11992ca1116298a887c41
Michael Berry <berryintlonline@gmail.com>
2018-04-02 09:34:42 -0600

    Finish Sellers and Subscriptions API

---
+++ src/Sellers/ListMarketplaceParticipations.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AmazonMWSAPI\Sellers;
+
+class ListMarketplaceParticipations extends Sellers
+{
+
+    protected static $requestQuota = 15;
+    protected static $restoreRate = 1;
+    protected static $restoreRateTime = 1;
+    protected static $restoreRateTimePeriod = "minute";
+    protected static $method = "POST";
+    private static $curlParameters = [];
+    private static $apiUrl = "http://docs.developer.amazonservices.com/en_US/sellers/Sellers_ListMarketplaceParticipations.html";
+    protected static $requiredParameters = [];
+    protected static $allowedParameters = [];
+    protected static $parameters = [
+        "SellerId" => [
+            "required"
+        ]
+    ];
+
+}
---
+++ src/Sellers/ListMarketplaceParticipationsByNextToken.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AmazonMWSAPI\Sellers;
+
+class ListMarketplaceParticipationsByNextToken extends Sellers
+{
+
+    protected static $requestQuota = 15;
+    protected static $restoreRate = 1;
+    protected static $restoreRateTime = 1;
+    protected static $restoreRateTimePeriod = "minute";
+    protected static $method = "POST";
+    private static $curlParameters = [];
+    private static $apiUrl = "http://docs.developer.amazonservices.com/en_US/sellers/Sellers_ListMarketplaceParticipationsByNextToken.html";
+    protected static $requiredParameters = [];
+    protected static $allowedParameters = [];
+    protected static $parameters = [
+        "NextToken" => [
+            "required"
+        ],
+        "SellerId" => [
+            "required"
+        ]
+    ];
+
+}
